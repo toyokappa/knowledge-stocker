8444c56466bd88a8bfa12eaeb8eeb5e873e012c1
toyokappa <kppg42@gmail.com>
2017-07-08 20:26:42 +0900

    Update user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "登録が完了しました！"
+      log_in(@user)
       redirect_to @user
     else
       render "new"
