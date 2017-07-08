ab42823f7d5d65efc1e59a72fd93918edbcb663c
toyokappa <kppg42@gmail.com>
2017-07-08 19:30:52 +0900

    Add create and show action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,22 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "登録が完了しました！"
+      redirect_to @user
+    else
+      render "new"
+    end
   end
 
   def show
+    @user = User.find(params[:id])
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
