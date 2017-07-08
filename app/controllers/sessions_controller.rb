7e5a443f3aadf33086e1a847fed42e1157bf2471
toyokappa <kppg42@gmail.com>
2017-07-08 20:18:47 +0900

    Update sessions controller for login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      log_in(user)
+      redirect_to user
+    else
+      flash.now[:danger] = "メールアドレスもしくはパスワードが間違っています。"
+      render "new"
+    end
   end
 
   def destroy
+    log_out
+    redirect_to root_path
   end
 end
