cc145cec91c46acbb1188c6d06baf9dc21a929bb
toyokappa <kppg42@gmail.com>
2017-07-09 10:20:23 +0900

    Define words table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170708094327) do
+ActiveRecord::Schema.define(version: 20170709010432) do
 
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
@@ -19,6 +19,14 @@ ActiveRecord::Schema.define(version: 20170708094327) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
+  create_table "words", force: :cascade do |t|
+    t.string "content", null: false
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_words_on_user_id"
   end
 
 end
