dc7bd15b9c02e08c70d1948c9da486fe06147b90
toyokappa <kppg42@gmail.com>
2017-07-09 12:28:13 +0900

    Add word delete link

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,5 +1,5 @@
 class WordsController < ApplicationController
-  before_action :logged_in_user, only: [:index, :new, :create, :show]
+  before_action :logged_in_user
 
   def index
     @words = Word.where(user_id: current_user)
