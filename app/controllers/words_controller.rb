3a442a34914ad9ce2d78d2405a35e2bfcd1c1d26
toyokappa <kppg42@gmail.com>
2017-07-09 11:24:19 +0900

    Add show action to words controller

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,5 +1,5 @@
 class WordsController < ApplicationController
-  before_action :logged_in_user, only: [:new, :create]
+  before_action :logged_in_user, only: [:new, :create, :show]
 
   def new
     @word = current_user.words.build
@@ -9,10 +9,14 @@ class WordsController < ApplicationController
     @word = current_user.words.build(word_params)
     if @word.save
       flash[:success] = "単語登録が完了しました！"
-      redirect_to root_url
+      redirect_to @word
     else
       render "new"
     end
+  end
+
+  def show
+    @word = Word.find(params[:id])
   end
 
   private
