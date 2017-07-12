1ef3a9211a9a5aedd5cc01166ba98a48a246da96
toyokappa <kppg42@gmail.com>
2017-07-12 21:19:32 +0900

    Update validation for email format

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  has_secure_password
   has_many :words
 
   validates :name, presence: true, length: { maximum: 30 }
-  validates :email, presence: true, length: { maximum: 255 },
-                    uniqueness: true
+  validates :email, presence: true, uniqueness: true,
+                    email_format: { message: "正しいメールアドレスの形式で入力してください。" }
   validates :password, presence: true, length: { minimum: 6 }
+  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -35,9 +35,22 @@ RSpec.describe User, type: :model do
       expect(@user.valid?).to eq false
     end
 
-    it "has 255 characters or less" do
-      @user.email = "a" * 244 + "@example.com"
-      expect(@user.valid?).to eq false
+    it "has valid format" do
+      valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                           first.last@foo.jp alice+bob@baz.cn]
+      valid_addresses.each do |valid_address|
+        @user.email = valid_address
+        expect(@user.valid?).to eq true
+      end
+    end
+
+    it "has invalid format" do
+      invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                             foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+      invalid_addresses.each do |invalid_address|
+        @user.email = invalid_address
+        expect(@user.valid?).to eq false
+      end
     end
   end
 
