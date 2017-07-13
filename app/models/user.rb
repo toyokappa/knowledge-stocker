d8d83e7ce48769c35144cbe7ea96d02341888a00
toyokappa <kppg42@gmail.com>
2017-07-13 21:27:20 +0900

    Update email validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, uniqueness: true,
-                    email_format: { message: "正しいメールアドレスの形式で入力してください。" }
+                    email_format: true
   validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
 end
