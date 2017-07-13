36f95e0a01d3612e69b645149f17162c2f628c81
toyokappa <kppg42@gmail.com>
2017-07-14 00:35:29 +0900

    Change user spec with shoulda-matchers

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,65 +1,26 @@
 require 'rails_helper'
+require 'validates_email_format_of/rspec_matcher'
 
 RSpec.describe User, type: :model do
   let(:user) { build(:user) }
+  subject { user }
 
-  it "is valid" do
-    expect(user).to be_valid
-  end
+  it { is_expected.to be_valid }
 
   describe "#name" do
-    it "is present" do
-      user.name = "  "
-      expect(user).to be_invalid
-    end
-
-    it "has 30 characters or less" do
-      user.name = "a" * 31
-      expect(user).to be_invalid
-    end
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_length_of(:name).is_at_most(30) }
   end
 
   describe "#email" do
-    it "is present" do
-      user.email = "  "
-      expect(user).to be_invalid
-    end
-
-    it "is unique" do
-      duplication_user = user.dup
-      duplication_user.email.upcase
-      duplication_user.save
-      expect(user).to be_invalid
-    end
-
-    it "has valid format" do
-      valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
-                           first.last@foo.jp alice+bob@baz.cn]
-      valid_addresses.each do |valid_address|
-        user.email = valid_address
-        expect(user).to be_valid
-      end
-    end
-
-    it "has invalid format" do
-      invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
-                             foo@bar_baz.com foo@bar+baz.com foo@bar..com]
-      invalid_addresses.each do |invalid_address|
-        user.email = invalid_address
-        expect(user).to be_invalid
-      end
-    end
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_uniqueness_of(:email) }
+    it { is_expected.to validate_email_format_of(:email).
+                        with_message('は不正な形式です') }
   end
 
   describe "#password" do
-    it "is present" do
-      user.password = user.password_confirmation = " " * 6
-      expect(user).to be_invalid
-    end
-
-    it "has 6 characters or more" do
-      user.password = user.password_confirmation = "a" * 5
-      expect(user).to be_invalid
-    end
+    it { is_expected.to have_secure_password }
+    it { is_expected.to validate_length_of(:password).is_at_least(6) }
   end
 end
