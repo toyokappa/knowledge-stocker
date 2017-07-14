b7f44d4b7b8f821fe45e6e59d40c4f27157dbbd6
toyokappa <kppg42@gmail.com>
2017-07-14 17:03:27 +0900

    Fix for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   has_many :words, dependent: :destroy
 
   validates :name, presence: true, length: { maximum: 30 }
-  validates :email, presence: true, uniqueness: true,
-                    email_format: true
+  validates :email, presence: true, uniqueness: true, email_format: true
   validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it "has valid factory" do
+  it "has a valid factory" do
     expect(build(:user)).to be_valid
   end
 
