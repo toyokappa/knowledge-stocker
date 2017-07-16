ef7f49d3e7ea9e0acb826c0af32fba8a0c012605
toyokappa <kppg42@gmail.com>
2017-07-16 10:11:43 +0900

    Add scope test to knowledge spec

--- spec/models/knowledge_spec.rb
+++ spec/models/knowledge_spec.rb
@@ -9,6 +9,21 @@ RSpec.describe Knowledge, type: :model do
     it { is_expected.to belong_to(:word) }
   end
 
+  describe 'scope' do
+    describe '.understanding_order' do
+      let!(:understanding_1) { create(:knowledge, understanding: 1) }
+      let!(:understanding_2) { create(:knowledge, understanding: 2) }
+      let!(:understanding_3) { create(:knowledge, understanding: 3) }
+      let!(:knowledge_order) { [understanding_3, understanding_2, understanding_1] }
+
+      subject(:relation) { described_class.understanding_order }
+
+      it 'returns sort order by understanding desc' do
+        expect(relation.ids).to eq knowledge_order.map(&:id)
+      end
+    end
+  end
+
   describe "validation" do
     it { is_expected.to validate_presence_of(:url)}
     it { is_expected.to ensure_valid_url_format_of(:url) }
