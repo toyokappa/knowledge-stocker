445dc7f483d74175dddc380d019ac1a2d53818e9
toyokappa <kppg42@gmail.com>
2017-07-22 09:20:18 +0900

    Add annotate to word model

--- spec/factories/words.rb
+++ spec/factories/words.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: words
+#
+#  id         :integer          not null, primary key
+#  content    :string           not null
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 FactoryGirl.define do
   factory :word do
     content { Faker::Lorem.word }
--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: words
+#
+#  id         :integer          not null, primary key
+#  content    :string           not null
+#  user_id    :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 require 'rails_helper'
 
 RSpec.describe Word, type: :model do
