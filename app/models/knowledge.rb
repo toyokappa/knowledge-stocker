fe578bc7f6ad5a5295c7686568dfea4d7ddd6030
toyokappa <kppg42@gmail.com>
2017-07-22 09:20:47 +0900

    Add annotate to knowledge model

--- app/models/knowledge.rb
+++ app/models/knowledge.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: knowledges
+#
+#  id            :integer          not null, primary key
+#  url           :string           not null
+#  word_id       :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  understanding :integer          default(0)
+#
+
 class Knowledge < ApplicationRecord
   belongs_to :word
   scope :understanding_order, -> { order(understanding: :desc) }
--- spec/models/knowledge_spec.rb
+++ spec/models/knowledge_spec.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+#
+# Table name: knowledges
+#
+#  id            :integer          not null, primary key
+#  url           :string           not null
+#  word_id       :integer
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  understanding :integer          default(0)
+#
+
 require 'rails_helper'
 
 RSpec.describe Knowledge, type: :model do
