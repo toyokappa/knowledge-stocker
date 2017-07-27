a1569294de618aabd2f3c27accf71a5d0fb25a0a
toyokappa <kppg42@gmail.com>
2017-07-28 00:14:04 +0900

    Change generators not to create helper

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module KnowledgeStocker
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.generators do |g|
+      g.helper false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
