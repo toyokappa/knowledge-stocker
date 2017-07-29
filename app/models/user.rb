67dcbaaff2f8c1a234eeca5a9fecbddd88bb4f15
toyokappa <kppg42@gmail.com>
2017-07-29 11:45:04 +0900

    Delete allow_nil validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -19,6 +19,6 @@ class User < ApplicationRecord
 
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, uniqueness: true, email: true
-  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+  validates :password, presence: true, length: { minimum: 6 }
   has_secure_password
 end
