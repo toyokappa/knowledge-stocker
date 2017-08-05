fc2c405cf6c6277a92029de0af15113854eb09fd
toyokappa <kppg42@gmail.com>
2017-08-05 11:20:00 +0900

    Fix user create action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,6 +13,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  def create_session(user)
+    session[:user_id] = user.id
+  end
+
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
