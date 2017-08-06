2f7dd55835abf949d4ae8eb014b9f01ad220c9e4
toyokappa <kppg42@gmail.com>
2017-08-06 17:51:14 +0900

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 
-  resource :user, format: false, except: [:new, :create, :destroy]
+  resource :user, only: [:show, :edit, :update]
   resources :words
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
