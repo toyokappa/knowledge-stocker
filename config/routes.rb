257e27d53bf2569a1664bb2e7183590b6f58fa27
toyokappa <kppg42@gmail.com>
2017-08-06 11:24:30 +0900

    Arrange routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 
-  resource :user
+  resource :user, format: false, except: [:new, :create, :destroy]
   resources :words
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
