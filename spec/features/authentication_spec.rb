d6af5118a38fdf4944818f636b5790ea8a4a5aad
toyokappa <kppg42@gmail.com>
2017-08-13 22:36:03 +0900

    Update authentication spec

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -1,22 +1,39 @@
 require "rails_helper"
 
-feature "Authentication" do
-  given(:user) { create(:user) }
-  given(:invalid_user) { build(:invalid_user) }
+feature "認証" do
+  feature "ログイン" do
+    before do
+      visit login_path
+      fill_in User.human_attribute_name(:email), with: user.email
+      fill_in User.human_attribute_name(:password), with: user.password
+      click_button I18n.t("link.login")
+    end
 
-  scenario "login failed" do
-    log_in_as(invalid_user)
-    expect(page).to have_content I18n.t("flash.authentication_failed")
+    context "登録済みユーザーの場合" do
+      given(:user) { create :user }
+      scenario "ログインに成功する" do
+        expect(current_path).to eq root_path
+        expect(page).to have_link I18n.t("link.logout")
+        visit user_path
+        expect(page).to have_content user.name
+        expect(page).to have_content user.email
+      end
+    end
+
+    context "未登録ユーザーの場合" do
+      given(:user) { build :user, email: "invalid@example.com", password: "invalid"}
+      scenario "ログインに失敗する" do
+        expect(page).to have_content I18n.t("flash.authentication_failed")
+      end
+    end
   end
 
-  scenario "login success" do
-    log_in_as(user)
-    expect(page).to have_link I18n.t("link.logout")
-  end
-
-  scenario "logout success" do
-    log_in_as(user)
-    click_link I18n.t("link.logout")
-    expect(page).to have_link I18n.t("link.login")
+  feature "ログアウト" do
+    given(:user) { create :user }
+    scenario "ログアウトに成功する" do
+      log_in_as user
+      click_link I18n.t("link.logout")
+      expect(page).to have_link I18n.t("link.login")
+    end
   end
 end
