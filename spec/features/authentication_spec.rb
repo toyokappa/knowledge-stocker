43f3e94e74a6714eeae3546b5b2372fb2372b706
toyokappa <kppg42@gmail.com>
2017-08-18 22:37:10 +0900

    Add invalid pattern to authentication spec

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -9,8 +9,9 @@ feature "認証" do
       click_button I18n.t("link.login")
     end
 
-    context "登録済みユーザーの場合" do
+    context "登録済みユーザーの情報を入力した場合" do
       given(:user) { create :user }
+
       scenario "ログインに成功する" do
         expect(current_path).to eq root_path
         expect(page).to have_link I18n.t("link.logout")
@@ -20,8 +21,25 @@ feature "認証" do
       end
     end
 
-    context "未登録ユーザーの場合" do
+    context "Emailとパスワードが未入力の場合" do
+      given(:user) { build :user, email: nil, password: nil}
+
+      scenario "ログインに失敗する" do
+        expect(page).to have_content I18n.t("flash.authentication_failed")
+      end
+    end
+
+    context "登録済みユーザーのEmailと不正なパスワードを入力した場合" do
+      given(:user) { build :user, password: "invalid"}
+
+      scenario "ログインに失敗する" do
+        expect(page).to have_content I18n.t("flash.authentication_failed")
+      end
+    end
+
+    context "未登録ユーザーのEmailとパスワードを入力した場合" do
       given(:user) { build :user, email: "invalid@example.com", password: "invalid"}
+
       scenario "ログインに失敗する" do
         expect(page).to have_content I18n.t("flash.authentication_failed")
       end
@@ -30,6 +48,7 @@ feature "認証" do
 
   feature "ログアウト" do
     given(:user) { create :user }
+    
     scenario "ログアウトに成功する" do
       login_as user
       click_link I18n.t("link.logout")
