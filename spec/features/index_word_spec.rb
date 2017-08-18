d810dfa242e2d18faf4e4d030a1c5f0cfca8311c
toyokappa <kppg42@gmail.com>
2017-08-18 20:50:42 +0900

    Update index word spec

--- spec/features/index_word_spec.rb
+++ spec/features/index_word_spec.rb
@@ -1,15 +1,11 @@
 require "rails_helper"
 
 feature "単語の一覧" do
-  given(:user) { create :user, words: [word1, word2, word3] }
-  given(:word1) { create :word }
-  given(:word2) { create :word }
-  given(:word3) { create :word }
+  given(:user) { create :user, words: words }
+  given(:words) { create_list :word, 3 }
   scenario "登録された単語を一覧で表示する" do
     log_in_as user
     click_link I18n.t("title.word_index")
-    expect(page).to have_content word1.content
-    expect(page).to have_content word2.content
-    expect(page).to have_content word3.content
+    words.each { |word| expect(page).to have_content word.content }
   end
 end
