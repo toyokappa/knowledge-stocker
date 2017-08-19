ec6cf155c15f33124e1c5359fcc8c11fd888d16b
toyokappa <kppg42@gmail.com>
2017-08-19 16:21:04 +0900

    Update index word sepc

--- spec/features/index_word_spec.rb
+++ spec/features/index_word_spec.rb
@@ -1,11 +1,11 @@
 require "rails_helper"
 
 feature "単語の一覧" do
-  given(:user) { create :user, words: words }
-  given(:words) { create_list :word, 3 }
+  given(:user) { create :user }
+  before { @words = create_list(:word, 3, user: user) }
   scenario "登録された単語を一覧で表示する" do
     login_as user
     click_link I18n.t("title.word_index")
-    words.each { |word| expect(page).to have_content word.content }
+    @words.each { |word| expect(page).to have_content word.content }
   end
 end
