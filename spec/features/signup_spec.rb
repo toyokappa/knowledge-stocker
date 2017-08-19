e696e281fd36e6b4495f40a9581019bab990ee57
toyokappa <kppg42@gmail.com>
2017-08-19 11:50:39 +0900

    Update signup spec

--- spec/features/signup_spec.rb
+++ spec/features/signup_spec.rb
@@ -1,46 +1,67 @@
 require "rails_helper"
 
 feature "ユーザー登録" do
+  given(:user) { build :user }
   before do
     visit login_path
     click_link I18n.t("title.user_registration")
-    fill_in "user_name", with: user.name
-    fill_in "user_email", with: user.email
-    fill_in "user_password", with: user.password
-    fill_in "user_password_confirmation", with: user.password_confirmation
+    fill_in "user_name", with: name
+    fill_in "user_email", with: email
+    fill_in "user_password", with: password
+    fill_in "user_password_confirmation", with: password_confirmation
     click_button I18n.t("helpers.submit.create")
   end
 
   context "正しい値が入力された場合" do
-    given(:user) { build :user }
+    given(:name) { Faker::Name.name }
+    given(:email) { Faker::Internet.unique.email }
+    given(:password) { "password" }
+    given(:password_confirmation) { "password" }
+
     scenario "登録できる" do
       expect(page).to have_content I18n.t("flash.registration_success")
     end
   end
 
   context "不正なユーザー名が入力された場合" do
-    given(:user) { build :user, name: " " }
+    given(:name) { nil }
+    given(:email) { user.email }
+    given(:password) { user.password }
+    given(:password_confirmation) { user.password_confirmation }
+
     scenario "登録できない" do
       expect(page).to have_content I18n.t("flash.errors_count", count: 1)
     end
   end
 
   context "不正なEmailが入力された場合" do
-    given(:user) { build :user, email: "invalid@example" }
+    given(:name) { user.name }
+    given(:email) { "invalid@email" }
+    given(:password) { user.password }
+    given(:password_confirmation) { user.password_confirmation }
+
     scenario "登録できない" do
       expect(page).to have_content I18n.t("flash.errors_count", count: 1)
     end
   end
 
   context "不正なパスワードが入力された場合" do
-    given(:user) { build :user, password: "foo", password_confirmation: "foo" }
+    given(:name) { user.name }
+    given(:email) { user.email }
+    given(:password) { "foo" }
+    given(:password_confirmation) { "foo" }
+
     scenario "登録できない" do
       expect(page).to have_content I18n.t("flash.errors_count", count: 1)
     end
   end
 
   context "パスワードとパスワード（確認用）が一致しない場合" do
-    given(:user) { build :user, password: "foobar", password_confirmation: "foobaz" }
+    given(:name) { user.name }
+    given(:email) { user.email }
+    given(:password) { "foobar" }
+    given(:password_confirmation) { "foobaz" }
+
     scenario "登録できない" do
       expect(page).to have_content I18n.t("flash.errors_count", count: 1)
     end
