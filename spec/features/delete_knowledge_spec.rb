1f58a70f6b81c9e18d7a5e3bd079de5813ebafb8
toyokappa <kppg42@gmail.com>
2017-08-19 16:07:57 +0900

    Update delete knowledge spec

--- spec/features/delete_knowledge_spec.rb
+++ spec/features/delete_knowledge_spec.rb
@@ -3,14 +3,14 @@ require "rails_helper"
 feature "URLの削除" do
   given(:user) { create :user }
   given(:word) { create :word, user: user }
-  given!(:knowledge) { create :knowledge, word: word }
+  before { @knowledge = create(:knowledge, word: word) }
 
   scenario "削除できる" do
     login_as user
     visit edit_word_path(word)
     check "word_knowledges_attributes_0__destroy"
     click_button I18n.t("helpers.submit.update")
-    expect(page).not_to have_content knowledge.url
+    expect(page).not_to have_content @knowledge.url
     click_link I18n.t("title.word_index")
     expect(page).not_to have_content I18n.t("view.understood_site")
   end
