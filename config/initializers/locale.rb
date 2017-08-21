73e7df87b6e38e19a63d09b9db1acfa76db4fb04
toyokappa <kppg42@gmail.com>
2017-08-21 19:09:10 +0900

    Rubocop auto-correct to config/initializers

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -1,5 +1,5 @@
 # 翻訳文の置き場所を指定
-Rails.application.config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+Rails.application.config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
 
 # デフォルトのロケールを設定する
 Rails.application.config.i18n.default_locale = :ja
