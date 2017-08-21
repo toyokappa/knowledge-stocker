2a27a0e1def86783ef9d50948b46fabd2d5cbe3c
toyokappa <kppg42@gmail.com>
2017-08-21 19:10:54 +0900

    Rubocop auto-correct to spec/factories

--- spec/factories/knowledges.rb
+++ spec/factories/knowledges.rb
@@ -22,7 +22,7 @@
 FactoryGirl.define do
   factory :knowledge do
     url { Faker::Internet.url }
-    understanding { Faker::Number.between(0,5) }
+    understanding { Faker::Number.between(0, 5) }
     word
   end
 end
