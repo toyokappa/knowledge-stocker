cd67691fc90e723ccc3df7a6e2585115f92fd026
toyokappa <kppg42@gmail.com>
2017-08-25 22:57:08 +0900

    Rename static_pages to home

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "static_pages#home"
+  root "home#index"
   get "/signup", to: "users#new"
   post "/signup", to: "users#create"
   get "/login", to: "sessions#new"
