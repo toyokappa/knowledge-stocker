4a6eb520574e2c8679c23696bd4ce736587ec283
toyokappa <kppg42@gmail.com>
2017-08-30 07:40:09 +0900

    Update some controllers using without_knowledges

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @word = current_user.words.build
-    @words = current_user.words.id_order.limit(10)
+    @words = current_user.words.without_knowledges.id_order.limit(10)
   end
 end
