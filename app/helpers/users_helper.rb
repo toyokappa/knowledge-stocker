b48876b7206be5bd7e3c1e5719f60db9557b3abb
toyokappa <kppg42@gmail.com>
2017-09-03 19:30:59 +0900

    :cop: rubocop --auto-correct --only Style/ColonMethodCall

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   def get_gravatar_of(user, size: 80)
-    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag(gravatar_url, alt: user.name, class: "user_icon")
   end
