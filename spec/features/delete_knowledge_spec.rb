62cd21fea44876ae6924e71100986cd877c60c74
toyokappa <kppg42@gmail.com>
2017-09-07 23:45:16 +0900

    Change delete_knowledge_spec for new UI

--- spec/features/delete_knowledge_spec.rb
+++ spec/features/delete_knowledge_spec.rb
@@ -7,9 +7,10 @@ feature "URLの削除" do
 
   scenario "削除できる" do
     login_as user
-    visit edit_word_path(word)
-    check "word[knowledges_attributes][0][_destroy]"
-    click_button I18n.t("helpers.submit.update")
+    visit word_path(word)
+    click_link "URLを削除"
+    expect(page).to have_content I18n.t("flash.delete_success")
+    expect(current_path).to eq word_path(word)
     visit knowledges_path
     expect(page).not_to have_content word.content
     visit words_path
