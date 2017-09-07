97568087dd6da98fc3ecdb7068566ed92646db3e
toyokappa <kppg42@gmail.com>
2017-09-07 23:51:07 +0900

    Add destroy knolwedges route

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  get "/knowledges", to: "knowledges#index"
 
   resource :user, only: [:show, :edit, :update]
   resources :words, only: [:index, :create, :show, :edit, :update, :destroy]
+  resources :knowledges, only: [:index, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
