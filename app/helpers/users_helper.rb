9b59aa0c73e839bfabc010711c0e8e3ab7dbd3b1
toyokappa <kppg42@gmail.com>
2017-09-09 14:28:14 +0900

    Update users_helper

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,14 +5,14 @@ module UsersHelper
     image_tag(gravatar_url, alt: user.name, class: "user_icon")
   end
 
-  def knowledges_par_words
-    words = current_user.words.count
-    knowledges = current_user.words.with_knowledges.pluck(:id).count
+  def knowledges_par_words(user)
+    words = user.words.count
+    knowledges = user.words.with_knowledges.pluck(:id).count
     "#{knowledges} / #{words}"
   end
 
-  def average_understanding
-    knowledges = current_user.words.select("words.id, knowledges.understanding").joins(:knowledges).order("knowledges.understanding DESC").uniq
+  def average_understanding(user)
+    knowledges = user.words.select("words.id, knowledges.understanding").joins(:knowledges).order("knowledges.understanding DESC").uniq
     understandings = knowledges.map(&:understanding)
     average = (understandings.sum.to_f / understandings.size).round(1)
     average.nan? ? 0 : average
